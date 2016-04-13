1f972c0f84ed934b1a3830765a3e0ff79612b2e5
Heinrich Wendel <heinrich.wendel@gmail.com>
2016-04-13 15:43:55 +0800

    Make the chrome extension nicer

---
+++ chrome-extension/chrome/iprice.js
@@ -0,0 +1,46 @@
+
+function addCommas(nStr)
+{
+  nStr += '';
+  x = nStr.split('.');
+  x1 = x[0];
+  x2 = x.length > 1 ? '.' + x[1] : '';
+  var rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) {
+    x1 = x1.replace(rgx, '$1' + ',' + '$2');
+  }
+  return x1 + x2;
+}
+
+ $.ajax({
+    url: "https://vivid-inferno-7935.firebaseIO.com/metrics/ga.json",
+    context: document.body,
+    success: function(data){
+      
+      var sessions = data[0];
+      var increment = data[1];
+      var goal = data[2];
+      var day = data[3];
+      var time = data[4];
+
+      e = document.getElementById('goal');
+      e.innerHTML = addCommas(goal);
+
+      e = document.getElementById('day')
+      e.innerHTML = day;
+      
+      e = document.getElementById('time')
+      e.innerHTML = time;
+
+      e = document.getElementById('sessions');
+      e.innerHTML = addCommas(sessions);
+
+       window.setInterval(function(){
+        sessions = sessions + increment;
+
+        element = document.getElementById('sessions')
+        element.innerHTML = addCommas(parseInt(sessions));
+       }, 1000);
+    }
+});
+ 
