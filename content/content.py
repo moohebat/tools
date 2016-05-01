fe80b9954a64f2370f96ed72a9a4559f166f1e4e
Heinrich Wendel <heinrich.wendel@gmail.com>
2016-05-01 21:13:36 +0800

    v0.1 of the complete script

--- content/content.py
+++ content/content.py
@@ -1,50 +1,108 @@
 #!/usr/bin/python
+# pip install lxml
+
 import csv, codecs, json, re, sys
+from lxml import html, cssselect
+from HTMLParser import HTMLParser
 
 reload(sys)
-sys.setdefaultencoding("UTF-8")
+sys.setdefaultencoding("UTF-8")	
 
-def word_count(string):
-	word_list = re.findall(r"[\w']+", string)
-	return len(word_list)
+# Simple Counts
+def character_count(string):
+	string = HTMLParser().unescape(re.sub('<[^<]+?>', '', string).strip())
+	return len(string)
 	
-def paragraph_count(string):
-	#paragraph_count = re.findall(r"<br>|<br/>|</p>", string)
-	return 0	
+def word_count(string):
+	string = HTMLParser().unescape(re.sub('<[^<]+?>', '', string).strip())
+	matches = re.findall(r"[\w']+", string)
+	return len(matches)
+	
+def paragraph_count(dom):
+	elements = dom.cssselect("p")
+	count = 0
+	for e in elements:
+		if not (e.text and e.text.strip() == ""):
+			count = count + 1
+	elements = dom.cssselect("br")
+	for e in elements:
+		count = count + 1
+	return count
 
 def h1_count(string):
-	return 0
-
+	matches = re.findall(r"(</h1>)", string)
+	return len(matches)
+	
 def h2_count(string):
-	return 0
+	matches = re.findall(r"(</h2>)", string)
+	return len(matches)
 
 def h3_count(string):
-	return 0
+	matches = re.findall(r"(</h3>)", string)
+	return len(matches)
 
 def list_count(string):
-	return 0
+	matches = re.findall(r"(</ul>)|(</ol>)", string)
+	return len(matches)
 
 def table_count(string):
-	return 0
+	matches = re.findall(r"(</table>)", string)
+	return len(matches)
 
 def image_count(string):
-	return 0
+	matches = re.findall(r"(<img)", string)
+	return len(matches)
 
-def list_broken(string):
-	return 0
+def link_count(string):
+	matches = re.findall(r"(</a>)", string)
+	return len(matches)
 
-def table_broken(string):
-	return 0
-
-def image_broken(string):
-	return 0
-
-def space_broken(string):
-	return 0
+# HTML Rules
+def single_item_list(dom):
+	elements = dom.cssselect("ul, ol")
+	broken = 0
+	for e in elements:
+		sub = e.cssselect("li")
+		if len(sub) < 2:
+			broken = broken + 1
+	return broken
 	
+def empty_paragraph(dom):
+	elements = dom.cssselect("p")
+	broken = 0
+	for e in elements:
+		if e.text and e.text.strip() == "":
+			broken = broken + 1
+	return broken
+
+def missing_table_header(dom):
+	elements = dom.cssselect("table")
+	broken = 0
+	for e in elements:
+		sub = e.cssselect("th")
+		if len(sub) == 0:
+			broken = broken + 1
+	return broken
+
+def table_width_height(dom):
+	elements = dom.cssselect("table, tr, th, td")
+	broken = 0
+	for e in elements:
+		if (e.attrib and "style" in e.attrib) and ("width" in e.attrib['style'] or "height" in e.attrib['style']):
+			broken = broken + 1
+	return broken
+
+def image_not_cdn(dom):
+	elements = dom.cssselect("img")
+	broken = 0
+	for e in elements:
+		if "iprice-prod" in e.attrib['src']:
+			broken = broken + 1
+	return broken
+
 def get_field(field, row):
 	if field in row['fields']:
-		return row['fields'][field][0]
+		return str(row['fields'][field][0])
 	else:
 		return ''
 
@@ -55,34 +113,51 @@ def calculate_stats(data):
 		leftText = get_field('sideText', row)
 		bottomText = get_field('text', row)
 		
-		url = get_field('url', row)
-		if url:
-			product = 'shop'
-		else:
-			url = get_field('store.url', row)
-			product = 'coupon'
-		
 		if not topText and not bottomText and not leftText:
-			print "hallo"
 			continue
 		
 		stat = {}
-		stat['url'] = url
-		stat['product'] = product
-		stat['brand'] = get_field('source.brand', row)
-		stat['series'] = get_field('source.series', row)
-		stat['model'] = get_field('source.model', row)
-		stat['category'] = get_field('source.category', row)
-		stat['gender'] = get_field('source.gender', row)
-		stat['color'] = get_field('source.color', row)
+		stat['URL'] = get_field('url', row)
+		
+		type = row['_type']
+		if type in ['brand', 'category', 'filtered']:
+			stat['Product'] = 'Shop'
+			if type == 'filtered':
+				subProduct = [
+					'brand' if get_field('source.brand', row) else '',
+					'series' if get_field('source.series', row) else '',
+					'model' if get_field('source.model', row) else '',
+					'category' if get_field('source.category', row) else '',
+					'gender' if get_field('source.gender', row) in ['1','2'] else '',
+					'color' if get_field('source.color', row) else ''
+				]
+				stat['SubProduct'] = "-".join(filter(None, subProduct))
+			else:
+				stat['SubProduct'] = type
+		elif type in ['blog', 'page']:
+			stat['Product'] = 'Static'
+			stat['SubProduct'] = type
+		elif type in ['store', 'couponCategory']:
+			stat['Product'] = 'Coupon'
+			stat['SubProduct'] = type
+		else:
+			print sys.stderr > "Unknown type: %s" % type
+		
+		domTopText = html.fragment_fromstring(topText, create_parent="div")
+		domLeftText = html.fragment_fromstring(leftText, create_parent="div")
+		domBottomText = html.fragment_fromstring(bottomText, create_parent="div")
+		
+		stat['Top_Characters'] = character_count(topText)
+		stat['Left_Character'] = character_count(leftText)
+		stat['Bottom_Characters'] = character_count(bottomText)
 		
 		stat['Top_Words'] = word_count(topText)
 		stat['Left_Words'] = word_count(leftText)
-		stat['Bottom_words'] = word_count(bottomText)
+		stat['Bottom_Words'] = word_count(bottomText)
 		
-		stat['Top_Paragraphs'] = paragraph_count(topText)
-		stat['Left_Paragraphs'] = paragraph_count(leftText)
-		stat['Bottom_Paragraphs'] = paragraph_count(bottomText)
+		stat['Top_Paragraphs'] = paragraph_count(domTopText)
+		stat['Left_Paragraphs'] = paragraph_count(domLeftText)
+		stat['Bottom_Paragraphs'] = paragraph_count(domBottomText)
 		
 		stat['Top_H1'] = h1_count(topText)
 		stat['Left_H1'] = h1_count(leftText)
@@ -99,10 +174,13 @@ def calculate_stats(data):
 		stat['Bottom_List'] = list_count(bottomText)
 		stat['Bottom_Image'] = image_count(bottomText)
 		stat['Bottom_Table'] = table_count(bottomText)
-		stat['List_Broken'] = list_broken(bottomText)
-		stat['Image_Borken'] = image_broken(bottomText)
-		stat['Table_Borken'] = table_broken(bottomText)
-		stat['Space_Borken'] = space_broken(bottomText)
+		stat['Bottom_Links'] = link_count(bottomText)
+		
+		stat['Single_Item_List'] = single_item_list(domBottomText)
+		stat['Empty_Paragraph'] = empty_paragraph(domBottomText)
+		stat['Missing_Table_Header'] = missing_table_header(domBottomText)
+		stat['Table_Width_Height'] = table_width_height(domBottomText)
+		stat['Image_Not_CDN'] = image_not_cdn(domBottomText)
 		
 		stats.append(stat)
 		
@@ -117,6 +195,7 @@ def read_data(filename):
 
 def output(stats):
 	w = csv.DictWriter(sys.stdout,stats[0].keys())
+	w.writeheader()
 	w.writerows(stats)
 
 data = read_data(sys.argv[1])
--- content/download.sh
+++ content/download.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 # npm install -g elasticdump
-# TODO: downloads some files that are empty
-# TODO: in VN 127 text without URL but text
 
 DB=https://search-iprice-production-6-kqdyyfrc7tpaznra3ahtmgalwa.ap-southeast-1.es.amazonaws.com
 
-#for CC in id hk my  ph sg th vn
-for CC in vn
+for CC in id hk my ph sg th vn
 do
 
-	elasticdump --input=$DB/content_$CC --output=output/$CC.json --type=data --searchBody '{ "size": 10, "query" : { "filtered" : { "filter" : { "bool" : { "should" : [ { "exists" : { "field": "text" } }, { "exists" : { "field": "shortText" } }, { "exists" : { "field": "sideText" } } ] } }, "query" : { "bool" : { "should" : [ { "bool" : { "must_not" : { "term" : { "text": "" } } } }, { "bool" : { "must_not" : { "term" : { "shortText": "" } } } }, { "bool" : { "must_not" : { "term" : { "sideText": "" } } } } ] } } } }, "fields" : ["store.url", "url", "text", "shortText", "sideText", "source.brand", "source.category", "source.gender", "source.model", "source.series", "source.color" ] }' --limit=1000
+	elasticdump --input=$DB/content_$CC --output=output/$CC.json --type=data --searchBody '{ "query" : { "filtered" : { "filter" : { "bool" : { "must" : { "exists" : { "field": "url" } }, "should" : [ { "exists" : { "field": "text" } }, { "exists" : { "field": "shortText" } }, { "exists" : { "field": "sideText" } } ] } }, "query" : { "bool" : { "should" : [ { "bool" : { "must_not" : { "term" : { "text": "" } } } }, { "bool" : { "must_not" : { "term" : { "shortText": "" } } } }, { "bool" : { "must_not" : { "term" : { "sideText": "" } } } } ] } } } }, "fields" : ["url", "text", "shortText", "sideText", "source.brand", "source.category", "source.gender", "source.model", "source.series", "source.color" ]}' --limit=1000
 
 done
 
