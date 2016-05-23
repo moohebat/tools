0811774eef964170c074a1152a6debf971ff8ed5
Heinrich Wendel <heinrich.wendel@gmail.com>
2016-05-23 22:31:08 +0800

    Add support for CooD to search console scripts

--- search-console/search-console.py
+++ search-console/search-console.py
@@ -12,8 +12,58 @@ reload(sys)
 sys.setdefaultencoding("UTF-8")
 socket.setdefaulttimeout(10)
 
-GA_IDS = { 'SG':'75448761', 'MY':'75229758','ID':'75897118', 'PH':'75445974', 'TH':'79109064', 'VN':'75895336', 'HK':'75887160' }
-GSC_IDS = { 'SG':'iprice.sg', 'MY':'iprice.my', 'ID':'iprice.co.id', 'PH':'iprice.ph', 'TH':'ipricethailand.com', 'VN':'iprice.vn', 'HK':'iprice.hk' }
+GA_IDS = { 
+  'SG': {
+    'IPRICE' : '75448761'
+  },
+  'MY': {
+    'IPRICE' : '75229758',
+    'SAYS' : '110258562',
+    'JUICE' : '110251884'
+  },
+  'ID': {
+    'IPRICE' : '75897118'
+  },
+  'PH' : {
+    'IPRICE' : '75445974',
+    'RAPPLER' : '111816390'
+  }, 
+  'TH': {
+    'IPRICE' : '79109064'
+  },
+  'VN': {
+    'IPRICE' : '75895336' 
+  },
+  'HK': {
+    'IPRICE' : '75887160'
+  }
+}
+GSC_IDS = { 
+  'SG': {
+    'IPRICE' : 'iprice.sg'
+  },
+  'MY': {
+    'IPRICE' : 'iprice.my',
+    'SAYS' : 'coupon.says.com',
+    'JUICE' : 'www.juiceonline.com/promotions'
+  },
+  'ID': {
+    'IPRICE' : 'iprice.co.id'
+  },
+  'PH' : {
+    'IPRICE' : 'iprice.ph',
+    'RAPPLER' : 'coupons.rappler.com'
+  }, 
+  'TH': {
+    'IPRICE' : 'ipricethailand.com'
+  },
+  'VN': {
+    'IPRICE' : 'iprice.vn' 
+  },
+  'HK': {
+    'IPRICE' : 'iprice.hk'
+  }
+}
 
 argparser = argparse.ArgumentParser(add_help=False)
 argparser.add_argument('cc', type=str, help=('Country code).'))
@@ -22,9 +72,9 @@ argparser.add_argument('pages', type=int, help=('Top N pages).'))
 
 PAGE_SIZE = 5000
 
-def get_top_landing_pages(service, cc, week, n):
+def get_top_landing_pages(service, cc, website, week, n):
   data = service.data().ga().get(
-    ids='ga:' + GA_IDS[cc],
+    ids='ga:' + GA_IDS[cc][website],
     start_index='1',
     max_results=n,
     start_date=get_date(week)[0],
@@ -35,12 +85,12 @@ def get_top_landing_pages(service, cc, week, n):
     filters='ga:medium==organic').execute()
   return data['rows']
 
-def get_keyword_data(service, cc, week, url, protocol):
+def get_keyword_data(service, cc, website, week, url, protocol):
   retry = 1
   while retry >= 0:
     try:
       tmp = service.searchanalytics().query(
-        siteUrl = protocol + "://" + GSC_IDS[cc] + "/",
+        siteUrl = protocol + "://" + GSC_IDS[cc][website] + "/",
         body = {
           'startDate' : get_date(week)[0],
           'endDate' : get_date(week)[1],
@@ -48,7 +98,7 @@ def get_keyword_data(service, cc, week, url, protocol):
           'dimensionFilterGroups': [{
             'filters' : [{
               'dimension': 'page',
-              'expression' : protocol + "://" + GSC_IDS[cc] + "/" + urllib.quote(url.encode('utf-8')),
+              'expression' : protocol + "://" + GSC_IDS[cc][website] + "/" + urllib.quote(url.encode('utf-8')),
               'operator' : 'equals'
             }]
           }],
@@ -78,9 +128,9 @@ def initialize_service(argv, id):
       scope='https://www.googleapis.com/auth/' + id + '.readonly')
   return service
 
-def output(url, traffic, data):
+def output(cc, website, url, traffic, data):
   for row in data:
-    print '"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"' % (url, row['keys'][1], row['keys'][0], row['impressions'], row['clicks'], row['ctr'], row['position'], traffic)
+    print '"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"' % (cc, website, url, row['keys'][1], row['keys'][0], row['impressions'], row['clicks'], row['ctr'], row['position'], traffic)
 
 def main(argv):
   args = argparser.parse_args()
@@ -89,25 +139,28 @@ def main(argv):
 
   ga, gsc = initialize_service(argv, "analytics"), initialize_service(argv, "webmasters")
 
-  print '"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"' % ("url", "date", "keyword", "impressions", "clicks", "ctr", "position", "sessions (week)")
+  print '"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"\t"%s"' % ("cc", "website", "url", "date", "keyword", "impressions", "clicks", "ctr", "position", "sessions (week)")
   
   bar = Bar('Processing', max=args.pages, suffix ='%(percent).1f%% - %(eta)ds')
-  urls = get_top_landing_pages(ga, args.cc, args.week, args.pages)
-  for row in urls:
+  
+  for website in GA_IDS[args.cc]:
 
-    data = []
+    urls = get_top_landing_pages(ga, args.cc, website, args.week, args.pages)
+    for row in urls:
 
-    # we switched from http to https between week 3 and 4
-    if args.week <= 4 and args.cc != 'VN':
-      data.extend(get_keyword_data(gsc, args.cc, args.week, row[0][1:], "http"))
-    if args.week >=3 or args.cc == 'VN':
-      data.extend(get_keyword_data(gsc, args.cc, args.week, row[0][1:], "https"))
+      data = []
 
-    output(row[0], row[1], data)
+      # we switched from http to https between week 3 and 4
+      if (args.week <= 4 and args.cc != 'VN') or website != "IPRICE":
+        data.extend(get_keyword_data(gsc, args.cc, website, args.week, row[0][1:], "http"))
+      if (args.week >=3 or args.cc == 'VN') and website == "IPRICE":
+        data.extend(get_keyword_data(gsc, args.cc, website, args.week, row[0][1:], "https"))
 
-    bar.next()
-  bar.finish()
-    
+      output(args.cc, website, row[0], row[1], data)
+
+      bar.next()
+    bar.finish()
+      
   print >> sys.stderr, '# End: Keyword Data: %s, %s, %s, %s' % (args.cc, args.week, args.pages, datetime.datetime.now().time().isoformat())
 
 if __name__ == '__main__':
