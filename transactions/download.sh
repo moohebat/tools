460c871e29609c8c40ae64b36e6e2f618b33d9b8
Heinrich Wendel <heinrich.wendel@gmail.com>
2016-06-06 17:05:11 +0000

    Transactions: Small fixes to the scripts

--- transactions/download.sh
+++ transactions/download.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 DATE=$(date +"%Y%m%d%H%M")
 
@@ -9,7 +9,7 @@ mkdir $OUTPUT/$DATE
 
 for i in {1..12}
 do
-  python transactions.py 2015-$i all > $OUTPUT/transactions-2015-$i.csv 2>> $OUTPUT/$DATE/errors.log
+  export DISPLAY=:1; python transactions.py 2015-$i all > $OUTPUT/$DATE/transactions-2015-$i.csv 2>> $OUTPUT/$DATE/errors.log
   head -q -n1 $OUTPUT/$DATE/transactions-2015-$i.csv > $OUTPUT/$DATE/transactions-2015.csv
 done
 
@@ -18,7 +18,7 @@ head -q -n1 $OUTPUT/$DATE/transactions-2015.csv > $OUTPUT/$DATE/transactions.csv
 
 for i in {1..5}
 do
-  python transactions.py 2016-$i all > $OUTPUT/transactions-2016-$i.csv 2>> $OUTPUT/$DATE/errors.log
+  export DISPLAY=:1; python transactions.py 2016-$i all > $OUTPUT/$DATE/transactions-2016-$i.csv 2>> $OUTPUT/$DATE/errors.log
   head -q -n1 $OUTPUT/$DATE/transactions-2016-$i.csv > $OUTPUT/$DATE/transactions-2016.csv
 done
 
